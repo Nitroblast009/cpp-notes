dfc0e85f040996372bd018b3e59e39f29cce34f3
Faizoo <faizaan3424@gmail.com>
2022-04-23 22:24:28 -0400

    more move sem

--- move-semantics.cpp
+++ move-semantics.cpp
@@ -2,6 +2,7 @@
     - Move semantics: instead of making a copy of a temp rvalue passed in, can move that temp value instead
     - Move constructor shallow copies given object
     - At the end of move constructor, must make given object hollow cuz its destructor will delete the object and, by effect, the object returned by the move constructor (cuz it is a shallow copy)
+    - std::move will convert lvalue to rvalue reference instead of casting
 */
 
 #include <iostream>
@@ -66,7 +67,7 @@ int main()
     Obj o{"Hello, world!"};
     printObjCopy(o);                              // o gets copied
     printObjLvalueReference(o);                   // actual o object used, not copied (most efficient)
-    printObjRvalueReference(Obj("Suck my nuts")); // Takes in rvalue and is more efficient than copying
+    printObjRvalueReference(Obj("Message...")); // Takes in rvalue and is more efficient than copying
 
     std::cout << "---END OF PROGRAM---" << std::endl;
 }
